c6da41397ec039778c17acb7fb2e599057c5a94e
Anthony <wangonguanthony@gmail.com>
2025-04-16 11:41:17 +0300

    added navbar and css for navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: "<Home />"
+        element: <Home />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/about",
+        element: <About />
     }
 ])
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,12 +8,12 @@ function Login(){
                 <h1>Login</h1>
                 <form>
                     <div>
-                        <label for="username">username</label>
-                        <inpu id="username" type="text" name="username" placeholder="username"></inpu>
+                        <label for="username">Username</label>
+                        <input id="username" type="text" name="username" placeholder="username"></input>
                     </div>
                     <div>
                         <label for="Password">Password</label>
-                        <inpu id="password" type="password" name="password" placeholder="password"></inpu>
+                        <input id="password" type="password" name="password" placeholder="password"></input>
                     </div>
                     <br/>
                     <button type="submit">Submit</button>
